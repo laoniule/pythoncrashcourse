4acc4df781e840674d4387affd3fb4f0c86b468a
laoniule <niuxiaodong70@qq.com>
2018-11-19 22:50:25 +0800

    2018/11/19 22:50

--- Chapter09/car.py
+++ Chapter09/car.py
@@ -1,26 +1,25 @@
 class Car():
-    def __init__(self,make,model,year):
-        self.make=make
-        self.model=model
-        self.year=year
-        self.odometer_reader=0
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reader = 0
 
     def get_description_name(self):
-        long_name=self.make+ " " +self.model+" "+str(self.year)
+        long_name = self.make + " " + self.model+" "+str(self.year)
         return long_name
-
 
     def read_odometer(self):
         print("This car has run "+str(self.odometer_reader)+" mile{s}.")
 
-    def update_odometer(self,newmile):
-        if newmile>self.odometer_reader:
-            self.odometer_reader=newmile
+    def update_odometer(self, newmile):
+        if newmile > self.odometer_reader:
+            self.odometer_reader = newmile
         else:
             print("You can rollback an odometer.")
 
 
-newcar=Car("Audi","A4",2016)
+newcar = Car("Audi", "A4", 2016)
 print(newcar.get_description_name())
 newcar.update_odometer(30)
 newcar.read_odometer()
