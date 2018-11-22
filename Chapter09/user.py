7b9c3a4cee3dfa2da50ba84f93ce1eab816d723b
laoniule <niuxiaodong70@qq.com>
2018-11-22 19:47:45 +0800

    home 20181122 19:47

--- Chapter09/user.py
+++ Chapter09/user.py
@@ -1,24 +1,24 @@
 class User():
-    def __init__(self,firstname,lastname,sex,age):
-        self.firstname=firstname
-        self.lastname=lastname
-        self.sex=sex
-        self.age=age
-        self.fullname=self.firstname+" "+self.lastname
-
+    def __init__(self, firstname, lastname, sex, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.sex = sex
+        self.age = age
+        self.fullname = self.firstname+" "+self.lastname
 
     def describe_user(self):
-        print("User Info: ")
-     
-        print("Username: "+self.fullname+"\tSex: "+self.sex \
-            + "\tAge: "+str(self.age))
+        print("User Info: \n")
+        print("Username: "+self.fullname+"\tSex: " + self.sex + 
+            "\tAge: "+str(self.age))
 
-    def greet_user(self,greet):
+    def greet_user(self, greet):
         print(greet + self.fullname)
 
-user1=User("wang","li","woman",35)
+
+user1 = User("wang", "li", "woman", 35)
 user1.greet_user("Hello ")
 user1.describe_user()
- 
-user2=User("Niu","Xiaodong","Man",48)
+
+
+user2 = User("Niu", "Xiaodong", "Man", 48)
 user2.greet_user("How are you，")
