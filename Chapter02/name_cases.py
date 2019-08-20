974f31b1ca7315f9d87abc8b2b9c1da1d874f706
laoniule <niuxiaodong70@qq.com>
2019-08-20 19:12:28 +0800

    submit

---
+++ Chapter02/name_cases.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+a='eric'
+print('Hello ' + a.title() + ',Would your like to learn some python today?')
+print('Hello ' + a.upper() + ',Would your like to learn some python today?')
+famous_name="\t\n\nalbert einstain\n\t\n"
+print(famous_name.title().strip() + ' once said:"A person who never made a mistake never tried anything new".')
+
+print(famous_name.title().lstrip() + ' once said:"A person who never made a mistake never tried anything new".')
+print(famous_name.title().rstrip() + ' once said:"A person who never made a mistake never tried anything new".')
--- Chapter06/user.py
+++ Chapter06/user.py
@@ -1,8 +1,14 @@
 user_0={'username':'efermi',
+user_0={'username':'efermi',
+        'first':'enrico',
         'first':'enrico',
         'last':'fermi',}
+        'last':'fermi',}
+
 
 for key,value in user_0.items():
+for key,value in user_0.items():
+    print("\nkey: " +key)
     print("\nkey: " +key)
     print("value: "+value)
 
