fe835a4d1fc95aa2136afece55da9fe79e63ef61
laoniule <niuxiaodong70@qq.com>
2019-08-26 23:23:51 +0800

    201908262300

--- Chapter07/mountain_poll.py
+++ Chapter07/mountain_poll.py
@@ -1,16 +1,16 @@
-responses={}
+responses = {}
 
-polling_active=True
+polling_active = True
 
 while polling_active:
-    name=input("请输入你的姓名： ")
-    response=input("你们要去爬哪座山？ ")
+    name = input("请输入你的姓名： ")
+    response = input("你们要去爬哪座山？ ")
 
-    responses[name]=response
+    responses[name] = response
 
-    repeat=input("是否需要其他人回答？(Yes/no) ")
-    if repeat=="no":
-        polling_active=False
+    repeat = input("是否需要其他人回答？(Yes/no) ")
+    if repeat == "no":
+        polling_active = False
 print("\n--- 投票结果 ---")
-for name,response in responses.items():
+for name, response in responses.items():
     print(name+"要去爬"+response+".")
