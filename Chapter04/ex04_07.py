b51b3c9490f5d159d5306d44f52ce2728e4b2af3
laoniule <niuxiaodong70@qq.com>
2019-08-26 23:25:12 +0800

    201908262300

--- Chapter04/ex04_07.py
+++ Chapter04/ex04_07.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-threetimeslist=[]
-for k in range(1,21):
-    if k%3==0:
+threetimeslist = []
+for k in range(1, 21):
+    if k % 3 == 0:
         threetimeslist.append(k)
 
 for m in threetimeslist:
-    print(m) 
+    print(m)
