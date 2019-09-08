b996acbacf2e39179e263bc4b92fbde69c81008a
laoniule <niuxiaodong70@qq.com>
2019-09-08 20:40:42 +0800

    201909082040

---
+++ Chapter15/mpl_squares.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   mpl_squares.py
+@Time    :   2019/09/08 16:59:03
+@Author  :   Niu Xiaodong 
+@Version :   1.0
+@Contact :   xniu@msn.com
+@License :   (C)Copyright 2017-2018
+@Desc    :   None
+'''
+
+# here put the import lib
+
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25, 36]
+plt.plot(squares, linewidth=5)
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square Value", fontsize=14)
+plt.tick_params(axis="both", labelsize=14)
+plt.show()
