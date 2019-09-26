054a6a1b239f4c3f93fd26d0a6471d2b9cc36b7e
laoniule <niuxiaodong70@qq.com>
2019-09-26 18:02:13 +0800

    200909261800

--- Chapter17/python_repos.py
+++ Chapter17/python_repos.py
@@ -3,7 +3,7 @@
 '''
 @File    :   python_repos.py
 @Time    :   2019/09/25 10:26:27
-@Author  :   Niu Xiaodong 
+@Author  :   Niu Xiaodong
 @Version :   1.0
 @Contact :   xniu@msn.com
 @License :   (C)Copyright 2017-2018
@@ -17,16 +17,16 @@ import requests
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
 r = requests.get(url)
-print("Status Code: " , r.status_code)
+print("Status Code: ", r.status_code)
 
-reponse_dict=r.json()
+reponse_dict = r.json()
 print(reponse_dict.keys())
 print("Total Count: ", reponse_dict['total_count'])
 
-repo_dicts=reponse_dict['items']
+repo_dicts = reponse_dict['items']
 print("repositories returned: ", len(repo_dicts))
 
-## 取出第一个repository的内容，打印这个repository中的所有条目的数量
+# 取出第一个repository的内容，打印这个repository中的所有条目的数量
 # repo_dict=repo_dicts[0]
 # print("\nkeys:",len(repo_dict))
 
@@ -34,7 +34,7 @@ print("repositories returned: ", len(repo_dicts))
 #     print("\nkey:",key)
 #     print("\tvalue:",value)
 
-## 研究第一个repository
+# 研究第一个repository
 # print("\nSelected information about first repository:")
 # print("\nName:",repo_dict['name'])
 # print("\nOwner:",repo_dict['owner']['login'])
@@ -44,15 +44,15 @@ print("repositories returned: ", len(repo_dicts))
 # print("\nUpdated:",repo_dict['updated_at'])
 # print("\nDescription:",repo_dict['description'])
 
-print("----------------------------------------------------")
-## 查看每个repository的信息
+
+# 查看每个repository的信息
 print("\nSelected information about each repository")
 for repos_dict in repo_dicts:
     print("----------------------------------------------------")
-    print("\nName:",repos_dict['name'])
-    print("\nOwner:",repos_dict['owner']['login'])
-    print("\nStars:",repos_dict['stargazers_count'])
-    print("\nRepository:",repos_dict['html_url'])
-    print("\nCreated:",repos_dict['created_at'])
-    print("\nUpdated:",repos_dict['updated_at'])
-    print("\nDescription:",repos_dict['description'])
+    print("\nName:", repos_dict['name'])
+    print("\nOwner:", repos_dict['owner']['login'])
+    print("\nStars:", repos_dict['stargazers_count'])
+    print("\nRepository:", repos_dict['html_url'])
+    print("\nCreated:", repos_dict['created_at'])
+    print("\nUpdated:", repos_dict['updated_at'])
+    print("\nDescription:", repos_dict['description'])
