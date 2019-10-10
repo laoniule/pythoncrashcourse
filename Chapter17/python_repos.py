bab9017710fbcbf114a72bd6cbdfc0e6035870ae
laoniule <28562930+laoniule@users.noreply.github.com>
2019-10-10 23:29:03 +0800

    201910102328

--- Chapter17/python_repos.py
+++ Chapter17/python_repos.py
@@ -13,10 +13,15 @@
 # here put the import lib
 
 import requests
+import pygal
+from pygal.Style import LightColorizedStyle as LCS, LightenStyle as LS
 
-url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
-r = requests.get(url)
+
+
+URL = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+
+r = requests.get(URL)
 print("Status Code: ", r.status_code)
 
 reponse_dict = r.json()
@@ -24,6 +29,18 @@ print(reponse_dict.keys())
 print("Total Count: ", reponse_dict['total_count'])
 
 repo_dicts = reponse_dict['items']
+names,stars=[],[]
+names.append(repo_dictsp['name'])
+stars.append(repo_dictsp["Stargazers_count"])
+
+my_style = LS('#333366',base_style=LCS)
+chart=pygal.Bar(Style=my_style,x_label_rotation=45,show_legend=False)
+chart.title='Most-Starred Python Projects on Github'
+chart.x_labels=names
+chart.add(",stars")
+chart.render_to_file("python_repos.svg")
+
+
 print("repositories returned: ", len(repo_dicts))
 
 # 取出第一个repository的内容，打印这个repository中的所有条目的数量
@@ -36,23 +53,23 @@ print("repositories returned: ", len(repo_dicts))
 
 # 研究第一个repository
 # print("\nSelected information about first repository:")
-# print("\nName:",repo_dict['name'])
-# print("\nOwner:",repo_dict['owner']['login'])
-# print("\nStars:",repo_dict['stargazers_count'])
-# print("\nRepository:",repo_dict['html_url'])
-# print("\nCreated:",repo_dict['created_at'])
-# print("\nUpdated:",repo_dict['updated_at'])
-# print("\nDescription:",repo_dict['description'])
+# print("Name:",repo_dict['name'])
+# print("Owner:",repo_dict['owner']['login'])
+# print("Stars:",repo_dict['stargazers_count'])
+# print("Repository:",repo_dict['html_url'])
+# print("Created:",repo_dict['created_at'])
+# print("Updated:",repo_dict['updated_at'])
+# print("Description:",repo_dict['description'])
 
 
 # 查看每个repository的信息
-print("\nSelected information about each repository")
+print("Selected information about each repository")
 for repos_dict in repo_dicts:
     print("----------------------------------------------------")
-    print("\nName:", repos_dict['name'])
-    print("\nOwner:", repos_dict['owner']['login'])
-    print("\nStars:", repos_dict['stargazers_count'])
-    print("\nRepository:", repos_dict['html_url'])
-    print("\nCreated:", repos_dict['created_at'])
-    print("\nUpdated:", repos_dict['updated_at'])
-    print("\nDescription:", repos_dict['description'])
+    print("Name:", repos_dict['name'])
+    print("Owner:", repos_dict['owner']['login'])
+    print("Stars:", repos_dict['stargazers_count'])
+    print("Repository:", repos_dict['html_url'])
+    print("Created:", repos_dict['created_at'])
+    print("Updated:", repos_dict['updated_at'])
+    print("Description:", repos_dict['description'])
